3ea9ea3c275816e2ffb5bee815c21afc43bfc845
Ir0has <kick.des@gmail.com>
2017-04-04 11:06:36 +0900

    メニューの追加
    メニュー新規追加
    cssファイル追加
    トップからメニュー１へのリンク追加

---
+++ menu1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>hello</title>
+    <link rel="stylesheet" type="text/css" href="./css/menu1.css" media="all">
+  </head>
+  <body class="menu1title">
+    <div>
+      ƒƒjƒ…[‚P
+    </div>
+    <div>
+      ‚±‚±‚Å‚È‚ñ‚©‚·‚é—\’è’n
+    </div>
+  </body>
+</html>
