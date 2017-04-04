e5438d2962d0dab3a368e1cde9b800bbffdef816
karihata <karihata5526@gmail.com>
2017-04-04 13:56:35 +0900

    TABインデントとページ体系を整理
    ・TABインデントで視覚性を向上
    ・トップページに戻るリンクを左上に追加
    ・テーブルタグでコンテンツの段を管理
    ・記事全体を中央寄りに変更(トップページへのリンクを除く)

--- menu2.php
+++ menu2.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <title>PHPとGitHubの勉強用のページ(menu2.php)</title>
-    <link rel="stylesheet" type="text/css" href="./css/menu2.css" media="all">
-  </head>
-  <body class="menutitle">
-    <div>
-      メニュー２
-    </div>
-    <div>
-      PHPとGitHubの勉強用のページ()
-    </div>
-  </body>
+	<head>
+		<meta charset="UTF-8">
+		<title>karihataの勉強のページ</title>
+		<link rel="stylesheet" type="text/css" href="./css/menu2.css" media="all">
+	</head>
+	<body class="menu">
+		<div>
+			<a href="top.html">＜＜TOPページ</a>
+		</div>
+		<div align=center>
+			<table class="menutitle" border="1">
+				<tr>
+					<td>
+						<div>
+							メニュー２
+						</div>
+						<div>
+							～PHPとGitHubの勉強用のページ～
+						</div>
+						<div>
+							(ここに記事を記載予定)
+						</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
 </html>
